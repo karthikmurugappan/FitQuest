86a422e367f4e1257a2fbb2403f38df30cbbc0b4
david-leverenz <david.leverenz@gmail.com>
2023-07-28 16:26:25 -0500

    pages done (I hope correctly) mutations and queries empty files added

---
+++ client/src/components/ExerciseList/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useMutation } from '@apollo/client';
+
+// This page needs to be written (obviously).  Just creating this so I could create the profile.js. - Dave
+// Need to map over the exercises and render them on the page.
+
+export default ExerciseList;
