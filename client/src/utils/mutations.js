9b24a83c9029cffbd606b300b5113c6fe27e6c39
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-07-30 11:52:46 -0500

    working with queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,20 +2,48 @@ import { gql } from '@apollo/client';
 
 // Creates a profile.
 export const ADD_PROFILE = gql`
-
+mutation addProfile($name: String!, $email: String!, $password: String!) {
+    addProfile(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
 `;
 
 // Logs in a user.
 export const LOGIN_USER = gql`
-
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
 `;
 
 // Adds an exercise to a user.
 export const ADD_EXERCISE = gql`
-
+mutation addExercise($profileId: ID!, $exercise: String!) {
+    addExercise(profileId: $profileId, exercise: $excerise) {
+      _id
+      name
+      exercises
+    }
+  }
 `;
 
 // Removes an exercise from a user.
 export const REMOVE_EXERCISE = gql`
- 
+mutation removeExercise($exercise: String!) {
+    removeSkill(exercise: $exercise) {
+      _id
+      name
+      exercises 
+    }
+  }
 `;
