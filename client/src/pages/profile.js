63062f006b3af2c6d137b78461c10d187e38dc2c
david-leverenz <david.leverenz@gmail.com>
2023-08-01 17:31:18 -0500

    formatted the exercise cards

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+// Import the things necessary to render the page.
+import React, { useState } from 'react';
+import { Navigate, useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
+import { Card, Col, Container, Row, Form } from 'react-bootstrap';
+import './profile.css'
+import bluePotion from '../styles/img/icons/potion-blue.png'
 
-
-// This imports the Exercise List js file for use on the page.
-// import ExerciseList from '../components/ExerciseList';
 
 // This imports the two queries in use on the page.
-import { QUERY_ME, QUERY_EXERCISES } from '../utils/queries';
+import { QUERY_ME, ADD_, QUERY_EXERCISES } from '../utils/queries';
 
 // Auth function uses the token to identify the "me" user.
 import Auth from '../utils/auth';
 
+// This retrieves the exercise list.
 const Profile = () => {
 
     const { loading, data } = useQuery(QUERY_EXERCISES, {
@@ -26,93 +27,76 @@ const Profile = () => {
     });
 
     const userExerciseList = currentUserData?.exercises || [];
-    console.log(userExerciseList);
-
-    console.log(currentUserData);
 
     const userInfo = currentUserData?.me || {};
     // console.log(userInfo.user_id.email);
 
+    // This is the drop down functionality.
+    const [selectedCategory, setSelectedCategory] = useState('All');
+
+    const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value);
+    };
+
+    const filteredCards = selectedCategory === 'All' ? exerciseList : exerciseList.filter(card => card.type === selectedCategory);
+
+
     return (
-        <div>
-            {userDataLoading ? (<h2>Loading...</h2>) : (
-                <div className="row">
-                    <div className="col text-info text-center" >Strength:{userInfo.strength} </div>
-                    <div className="col text-white text-center" >Stamina:{userInfo.strength} </div>
-                    <div className="col text-white text-center" >Agility:{userInfo.strength} </div>
-                    {/* {userInfo.user_id.username}
-                    {userInfo.user_id.email}
-                    {userInfo.agility}
-                    {userInfo.strength}
-                    {userInfo.stamina} */}
-                </div>
-            )}
 
+        <Container className="rpgui-container framed-golden">
+            <div>
+                {userDataLoading ? (<h2>Loading...</h2>) : (
+                    <div className="row">
+                        <div className="col text-info text-center" >Your Strength:{userInfo.strength} 25</div>
+                        <div className="col text-warning text-center" >Your Stamina:{userInfo.strength} 25</div>
+                        <div className="col text-danger text-center" >Your Agility:{userInfo.strength} 25</div>
+                        {/* {userInfo.user_id.username}
+                 {userInfo.user_id.email}
+                 {userInfo.agility}
+                 {userInfo.strength}
+                 {userInfo.stamina} */}
+                    </div>
+                )}
 
-            {/* <div className="row">{ userInfo.user_id.email}</div> */}
-
-            <h2>Exercises:</h2>
-            <ul className="square">
-                <div className="row">
-                    <div className="col text-white text-center" >Exercise Name</div>
-                    <div className="col text-white text-center">Type</div>
-                    <div className="col-5 text-white text-center">Description</div>
-                    <div className="col text-white text-center">Points</div>
-                </div>
-                {exerciseList.map((exercise) => {
-                    return (
-                        <li key={exercise._id}>
-                            <Link to={{ pathname: `/exercise/${exercise._id}` }}>
-                                <div className="row">
-                                    <div className="col text-center">{exercise.exercise_name}</div>
-                                    <div className="col text-center">{exercise.type}</div>
-                                    <div className="col-5">{exercise.description}</div>
-                                    <div className="col text-center">{exercise.points}</div>
-                                </div>
+                <h1>Choose an Excercise Quest to Complete</h1>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Filter by Exercise Type:</Form.Label>
+                        <Form.Control className=" rpgui-dropdown-imp" as="select" value={selectedCategory} onChange={handleCategoryChange}>
+                            <option value="All">All</option>
+                            <option value="Strength">Strength</option>
+                            <option value="Stamina (Cardiovascular)">Stamina</option>
+                            <option value="Agility">Agilty</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
+                <Row className="flex-wrap overflow-auto">
+                    {filteredCards.map((exercise, _id) => (
+                        <Col xs={4} key={exercise._id}>
+                            <Link to={{ pathname: `/profile` }}>
+                                {/* <Link to={{ pathname: `/exercise/${exercise._id}` }}> */}
+                                {/* =========================
+This link will need to change.
+========================= */}
+                                <Card className="custom-card">
+                                    {/* <Card.Img variant="top" src={card.image} alt={card.title} /> */}
+                                    <Card.Body>
+                                        <Card.Title className="custom-title">{exercise.exercise_name}</Card.Title>
+                                        <div className="custom-type">Type: {exercise.type}</div><br></br>
+                                        <div className="custom-description">{exercise.description}</div> <br></br>
+                                        <div className="align-items-center">
+                                            <img src={bluePotion} className="custom-icon" alt="Blue Potion" />
+                                            <div className="custom-points">Potion: {exercise.points}p</div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
                             </Link>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+        </Container >
     );
-
-
-    //   const { username: userParam } = useParams();
-
-    //   console.log({ userParam }); // Updated variable name
-
-    //   const { loading, data } = useQuery(userParam ? QUERY_SINGLE_USER : QUERY_ME, {
-    //     variables: { id: userParam }, // Updated variable name
-    //   });
-
-    //   const profile = data?.me || data?.user || {};
-
-    //   if (AuthService.loggedIn() && AuthService.getProfile()._id === profile._id) {
-    //     // Updated property name
-    //     return <Navigate to="/profile" />;
-    //   }
-
-    //   if (loading) {
-    //     return <div>Loading...</div>;
-    //   }
-
-    //   if (!profile?.name) {
-    //     return (
-    //       <h4>
-    //         You need to be logged in to view your profile. Please use the navigation links above to sign up or log in and continue your FitQuest!
-    //       </h4>
-    //     );
-    //   }
-
-    //   return (
-    //     <div>
-    //       <h2 className="">
-    //         {userParam ? `${profile.name}'s` : 'Your'} exercise list:
-    //       </h2>
-    //       <ExerciseList /> {/* Render the ExerciseList component */}
-    //     </div>
-    //   );
 };
 
 export default Profile;
